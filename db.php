2237d043d40b72329ec5ce37a7f5cbc903338caa
tangkasR <tangkasrisdianto2@gmail.com>
2022-09-19 22:34:00 +0700

    GD4_C_0863

---
+++ db.php
@@ -0,0 +1,10 @@
+<?php
+    $host = "localhost";
+    $user = "root";
+    $pass = "";
+    $name = "GD4_C_0863";
+    $con = mysqli_connect($host,$user,$pass,$name);
+    if(mysqli_connect_errno()){
+        echo "Failed to connect : " . mysqli_connect_error();
+    }
+?>
